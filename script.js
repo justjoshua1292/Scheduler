5d1a12771e23bab28e41cbb59e54b65c62877b48
Joshua Harris <Justjoshua1292@gmail.com>
2021-07-05 13:46:19 -0400

    adding arrays to reflect time

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var hourArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"]
+
+
 for (let index = 0; index < 10; index++) {
     var newDiv= $("<div>")
     newDiv.addClass("time-block row")
@@ -5,7 +8,7 @@ for (let index = 0; index < 10; index++) {
 
     var newH4= $("<h4>")
     newH4.addClass("hour col-md-1") 
-    newH4.text("9am")
+    newH4.text(hourArray[index])
 
 
     var newTextarea= $("<textarea>")
